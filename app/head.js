1140fd11525d232a307bcd50bed2a0adc140148d
Hasan-aga <careerforma@gmail.com>
2023-02-02 16:14:03 +0300

    use external image for open graph

--- app/head.js
+++ app/head.js
@@ -4,7 +4,10 @@ export default function Head() {
       <title>مناشد</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Created by Hasan" />
-      <meta property="og:image" content="/myName.svg" />
+      <meta
+        property="og:image"
+        content="https://i.ytimg.com/vi/CegqnRgXBW0/sddefault.jpg"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
