9b8de6e703e985ca8ccc60cd574adc8794cfc2cc
Hasan-aga <careerforma@gmail.com>
2023-02-02 16:05:59 +0300

    try link preview image

--- app/head.js
+++ app/head.js
@@ -4,6 +4,7 @@ export default function Head() {
       <title>مناشد</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Created by Hasan" />
+      <meta property="og:image" content="/myName.svg" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
